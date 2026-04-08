580688d62cf7e004a75e03faba157a606bc40501
Segun Oladiran <sheghunoladiran9@gmail.com>
2026-04-08 21:07:22 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -14,7 +14,7 @@ var (
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	fmt.Println("Logs play")
 
 	// Uncomment the code below to pass the first stage
 	//
