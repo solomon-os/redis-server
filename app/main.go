109068de519821aede76f56b82c10f2520b99292
Segun Oladiran <sheghunoladiran9@gmail.com>
2026-04-09 09:45:09 +0100

    implmented echo

--- app/main.go
+++ app/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
+	"log/slog"
 	"net"
 	"os"
+
+	"github.com/codecrafters-io/redis-starter-go/app/internal/parser"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
@@ -41,14 +44,29 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
+	buf := make([]byte, 1024)
+	p := parser.New()
 
-	scanner := bufio.NewScanner(conn)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Printf("%v", err)
+			return
+		}
 
-	for scanner.Scan() {
-		text := scanner.Text()
-		fmt.Println(text)
-		if text == "PING" {
-			_, _ = conn.Write([]byte("+PONG\r\n"))
+		p, err = p.Parse(string(buf[:n]), n)
+		if err != nil {
+			slog.Error("couldn't parse message: ", "error", err)
+			return
+		}
+		resp := p.Decode()
+		fmt.Printf("%#v", p)
+		fmt.Printf("%q\n", []byte(resp))
+
+		_, err = io.WriteString(conn, resp)
+		if err != nil {
+			slog.Error("couldn't send response: ", "error", err)
+			return
 		}
 	}
 }
